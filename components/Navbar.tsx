173fce6fbc1095b00c6284c0eb93d6a8743c17a9
Loskepetos <103402579+loskeptos@users.noreply.github.com>
2022-06-20 18:04:56 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Logo from '../src/logoivaa.svg'
 export const Navbar = () => {
     return (
 
-    <nav className='flex justify-between border border-b divide-purple-100'>
+    <nav className='flex justify-between border-b divide-purple-100'>
       <div className='flex items-center w-[25%] p-4 bg-purple-100'>
         <h1 className='flex-1 text-xl font-bold ml-4 '>Rumah IVAA</h1>
       </div>
